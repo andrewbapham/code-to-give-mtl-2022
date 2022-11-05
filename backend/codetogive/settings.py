3e30873f3b3fdd70504c2a10b096e88c9a49bba0
Anish George <anish.george2004@gmail.com>
2022-11-05 14:24:06 -0400

    Established firebase connectivity

--- backend/codetogive/settings.py
+++ backend/codetogive/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3v^e$h4(ve&y80_p3_t^8^5s$va%#=6kf0ucg#$o^wpd&%@1n1'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- backend/mainapp/views.py
+++ backend/mainapp/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+import pyrebase
+from decouple import config
+
+#Firebase Configs
+
+firebase_config ={
+    "apiKey": config('API_KEY'),
+    "authDomain":  config('AUTH_DOMAIN'),
+    "projectId":  config('PROJECT_ID'),
+    "databaseURL":  config('DB_URL'),
+    "storageBucket":  config('STORAGE_BUCKET'),
+    "messagingSenderId":  config('MESSAGING_SENDER_ID'),
+    "appId":  config('APP_ID'),
+    "measurementId":  config('MEASUREMENT_ID')
+}
+
+# Initialize Firebase
+firebase = pyrebase.initialize_app(firebase_config)
+auth = firebase.auth()
+db = firebase.database()
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse('Hello')
+    user_type = db.child('User').child('Type').get().val()
+    return JsonResponse({"UserType": user_type})
